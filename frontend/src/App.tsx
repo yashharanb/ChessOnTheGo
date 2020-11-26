3efe8ca8c5c4f4895a7d5c997771130fc34dc9ea
nicole-ec <nicole.caballero@ucalgary.ca>
2020-11-26 14:00:47 -0700

    started some routing, css, changed logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootswatch/dist/slate/bootstrap.min.css';
-import {PlayerMainMenu} from './MainMenu/PlayerMainMenu';
-
+import { PlayerMainMenu } from './MainMenu/PlayerMainMenu';
+import { PlayerStatistics } from "./PlayerStatistics";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App d-flex justify-content-center h-100 align-middle">
-      <PlayerMainMenu/>
-    </div>
+    <Router>
+      <div className="App d-flex justify-content-center h-100 align-middle">
+        <Route path="/menu" component={PlayerMainMenu} />
+        <Route path="/PlayerStatistics" component= {PlayerStatistics}/>
+      </div>
+    </Router>
   );
 }
 
