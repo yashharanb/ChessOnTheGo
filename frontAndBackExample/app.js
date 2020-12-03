252fb5214ed35db0736cc1a9faa0efcc23092880
karynnglli <karynngl.li@ucalgary.ca>
2020-12-03 00:53:54 -0700

    My passortjs, mongoose, bcrypt and ejs front and back end fully working prototype/example

---
+++ frontAndBackExample/app.js
@@ -0,0 +1,65 @@
+//As Kevin wants me to use express:
+const express = require('express');
+const app = express();
+//for the GUI using EJS in the views folder
+const expressLayouts = require('express-ejs-layouts');
+
+//for mongoDB Atlas
+const mongoose = require('mongoose');
+require("dotenv/config");
+
+//for the express session middleware
+const flash = require('connect-flash');
+const session = require('express-session');
+
+//for passpport.js config called in the "//Needed by passport.js middleware in the config folder and has to be after the //Express session section in here" section
+const passport = require('passport');
+require('./config/passport')(passport);
+
+//Connect using mongoose to MongoDB Atlas by calling the .env enviroment variable setup for each developer for now
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
+	.then(() => console.log('mongoDB Atlas Connected!'))
+	.catch(err => console.log(err));
+
+//for middle layer using EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Body parser (to get data from the form)
+app.use(express.urlencoded({extended: false}));
+app.use(express.static("public")); //help from https://stackoverflow.com/questions/17755147/displaying-an-image-with-ejs-in-node-js-express to put pictures accessible from public folder
+
+//Express session from https://github.com/expressjs/session#readme
+app.use(session(
+{
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+//Needed by passport.js middleware in the config folder and has to be after the //Express session section in here
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Connect flash
+app.use(flash());
+
+//Global variables for flashing success colors and error colors
+app.use(function(req, res, next)
+{
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+ });
+  
+//Routes
+app.use('/', require('./routes/index'));		//main page
+app.use('/users', require('./routes/users'));	//users page
+
+//Port that this will be run on
+const PORT = process.env.PORT || 3000;
+
+//Starts the server on specific port
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
