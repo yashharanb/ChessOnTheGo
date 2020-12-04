8e9bd768763367156726b590b6c3b0e09da10c9a
Yesha Patel <yesha.patel1998@yahoo.ca>
2020-12-03 20:51:21 -0700

    GamePage

--- frontend/src/MainMenu/Game.tsx
+++ frontend/src/MainMenu/Game.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ChessBoard from 'chessboardjsx';
 import { useWindowResize } from "beautiful-react-hooks";
+import timer from './timer.png';
+
 const { useState } = React;
-
-
 export function Game() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
@@ -11,20 +11,46 @@ export function Game() {
     setWindowWidth(window.innerWidth);
   });
 
-  let calcWidth=({
-    screenWidth: window.innerWidth,
-    screenHeight: window.innerHeight
-  });
+
+
+
+  let calcWidth=({screenWidth,screenHeight}:any)=>{
+    if(document && document.getElementById('typehead')){
+      // @ts-ignore
+      return document.getElementById('typehead').clientWidth;
+    }else{
+      return 500;
+    }
+
+  }
+
+
+
 
 // Display the chess board
   return(
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <ChessBoard position="start"/>
-          {windowWidth}
+    <div className="container" id="typehead" >
+    <div className="row">
+    <div className="col">
+      <img src={timer} className="img-fluid" alt="timer" />
+      21:00
+    </div>
+    </div>
+
+      <div className="row" >
+        <div className="col p-0"  >
+          <ChessBoard position="start" calcWidth={calcWidth}/>
         </div>
       </div>
+
+      <div className="row">
+      <div className="col">
+        <img src={timer} className="img-fluid" alt="timer" />
+        11:21
+      </div>
+    </div>
+
+
     </div>
   );
 
